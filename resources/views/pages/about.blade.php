0e4410a2f1e2b6c073f1582f28db45783a98043e
praptip1501-ai <praptip.1501@gmail.com>
2026-06-06 12:12:20 +0530

    about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <section class="py-5 mt-5">
-        <div class="container">
-            <h1>About Page</h1>
-        </div>
-    </section>
-
+@include('sections.about.about')
+@include('sections.about.review')
 @endsection
+
+
+
+
