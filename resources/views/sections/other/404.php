39b464e9e416aef4f59d13658efbc7636155d995
mahalleavanti-sys <mahalleavanti@gmail.com>
2026-06-09 17:59:02 +0530

    Changes made in 404 page

--- resources/views/sections/other/404.php
+++ resources/views/sections/other/404.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="not-found">
             <h1>404 - Page Not Found</h1>
-            <img src="/assets/images/error.png" alt="Page Not Found">
+            <img src="/assets/images/error.png" alt="Page Error Found">
             <p>Sorry, the page you're looking for doesn't exist or has been moved. Let's get you back on track.</p>
             <p>Andraos Construction provides comprehensive concrete and masonry services throughout the Denver metro area and Northern Colorado. Explore our service areas or get in touch with our team to learn more about our projects and capabilities.</p>
             <p>If you have questions about our services or need help finding the right page, visit our <a href="{{ url('/') }}">home page</a> or <a href="{{ url('/contact') }}">contact us</a> directly.</p>
