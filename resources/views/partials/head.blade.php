ea4e92fc74b96212b1a594a2b283c281c66a2f71
mahalleavanti-sys <mahalleavanti@gmail.com>
2026-06-12 10:30:05 +0530

    Added favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,7 +4,10 @@
 
     <title>Andraos Construction</title>
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+     <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/logo.png') }}">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
