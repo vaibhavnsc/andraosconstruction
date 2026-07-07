12a5c98811b4cf97835db22616f6d009702aff72
mahalleavanti-sys <mahalleavanti@gmail.com>
2026-07-07 13:02:28 +0530

    Work on mail integration

---
+++ app/Mail/AdminQuoteMail.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Quote;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class AdminQuoteMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Quote $quote;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Quote $quote)
+    {
+        $this->quote = $quote;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'New Quote Request',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.admin-quote',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
