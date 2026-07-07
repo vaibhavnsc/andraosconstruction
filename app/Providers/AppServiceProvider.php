427d98a0c6f7c3ed7a8026815026d0e9c59399e2
chandrakant <chandrakant.r.nagpure@gmail.com>
2026-07-07 16:43:04 +0530

    Fix LOCATIONS ReferenceError and Mixed Content HTTPS url scheme issues on staging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,11 +14,10 @@ class AppServiceProvider extends ServiceProvider
         require_once app_path('Helpers/seo.php');
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        if (str_starts_with(config('app.url'), 'https://')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
